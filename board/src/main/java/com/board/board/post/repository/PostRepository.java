c6207abd5152bc4021e685bedefa5da7f2bd23df
suhjaesuk <suhjaesuk@gmail.com>
2022-12-12 19:05:23 +0900

    Feat : PostRepository 생성날짜 기준 정렬

--- board/src/main/java/com/board/board/post/repository/PostRepository.java
+++ board/src/main/java/com/board/board/post/repository/PostRepository.java
@@ -1,4 +1,10 @@
 package com.board.board.post.repository;
 
-public class PostRepository {
+import com.board.board.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
 }
