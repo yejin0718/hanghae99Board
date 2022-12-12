00246fbd8972b275987819db631a596db330c9c6
suhjaesuk <suhjaesuk@gmail.com>
2022-12-12 18:33:38 +0900

    Feat : PostService

--- board/src/main/java/com/board/board/post/service/PostService.java
+++ board/src/main/java/com/board/board/post/service/PostService.java
@@ -1,4 +1,10 @@
 package com.board.board.post.service;
 
+import com.board.board.post.dto.PostResponseDto;
+
 public interface PostService {
+    public PostResponseDto getPostList();
+
+    public PostResponseDto getPostInfo(Long postId);
+
 }
