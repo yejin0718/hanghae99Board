c57d0480160798d4caae9852bfa9b7b4f22e16c1
suhjaesuk <suhjaesuk@gmail.com>
2022-12-12 19:06:47 +0900

    Feat : PostService 조회 기능 추가

--- board/src/main/java/com/board/board/post/service/PostService.java
+++ board/src/main/java/com/board/board/post/service/PostService.java
@@ -2,8 +2,10 @@ package com.board.board.post.service;
 
 import com.board.board.post.dto.PostResponseDto;
 
+import java.util.List;
+
 public interface PostService {
-    public PostResponseDto getPostList();
+    public List<PostResponseDto> getPostList();
 
     public PostResponseDto getPostInfo(Long postId);
 
