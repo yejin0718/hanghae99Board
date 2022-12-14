0b8775a085441423c64cc50822a34b5ef6479e51
faulty337 <faulty337@gmail.com>
2022-12-14 17:38:43 +0900

    Feat : 댓글 좋아요 기능 추가

--- board/src/main/java/com/board/board/like/service/LikeService.java
+++ board/src/main/java/com/board/board/like/service/LikeService.java
@@ -1,5 +1,8 @@
 package com.board.board.like.service;
 
+import com.board.board.comment.entity.Comment;
+import com.board.board.comment.repository.CommentRepository;
+import com.board.board.like.entity.LikeComment;
 import com.board.board.like.entity.LikePost;
 import com.board.board.like.repository.LikeCommentRepository;
 import com.board.board.like.repository.LikePostRepository;
@@ -17,9 +20,29 @@ public class LikeService {
     private final LikeCommentRepository likeCommentRepository;
     private final PostRepository postRepository;
 
+    private final CommentRepository commentRepository;
+
+
+    @Transactional
     public Boolean updateLikeComment(Long id, String username) {
-        return true;
+        LikeComment likeComment = likeCommentRepository.findByCommentIdAndUsername(id, username).orElse(null);
+        Comment comment = commentRepository.findById(id).orElseThrow(
+                ()-> new IllegalArgumentException("게시물을 찾을 수 없습니다."));
+
+        if(likeComment == null){
+            likeComment = new LikeComment();
+            likeComment.setUsername(username);
+            likeCommentRepository.save(likeComment);
+            comment.addLike(likeComment);
+            comment.increaseLike();
+            return true;
+        }else {
+            comment.reductionLike();
+            likeCommentRepository.delete(likeComment);
+            return false;
+        }
     }
+
     @Transactional
     public Boolean updateLikePost(Long id, String username) {
         LikePost likePost = likePostRepository.findByPostIdAndUsername(id, username).orElse(null);
