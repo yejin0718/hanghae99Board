6a5999fc80dc2e1c062ab01f7f8c4711a5baa67b
faulty337 <faulty337@gmail.com>
2022-12-15 10:14:21 +0900

    Feat : DB연결

--- board/src/main/java/com/board/board/global/config/WebSecurityConfig.java
+++ board/src/main/java/com/board/board/global/config/WebSecurityConfig.java
@@ -34,7 +34,6 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
